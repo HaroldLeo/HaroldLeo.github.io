2903bc2ec6fad468dc1e6bfaae88ce1e931aa606
Hongji Liu <h125678@gmail.com>
2020-10-12 18:43:47 -0500

    here

--- code.js
+++ code.js
@@ -1,67 +1,60 @@
 function getRandomColor() {
- 	 	 var letters = '0123456789ABCDEF'.split('');
- 	 	 var color = '#';
- 	 	 for (var i = 0; i < 6; i++ ) {
- 	 	 color += letters[Math.round(Math.random() * 15)];
- 	 	 }
- 	 	 return color;
-	 	 }
+	var letters = '0123456789ABCDEF'.split('');
+	var color = '#';
+	for (var i = 0; i < 6; i++) {
+		color += letters[Math.round(Math.random() * 15)];
+	}
+	return color;
+}
 
 var clickedTime; var createdTime; var reactionTime;
 function makeBox() {
 
-	 	 var time=Math.random();
+	var time = Math.random();
 
-	 	 time=time*5000;
+	time = time * 5000;
 
-			 setTimeout(function() {
+	setTimeout(function () {
 
-				 if (Math.random()>0.5) {
+		if (Math.random() > 0.5) {
+			document.getElementById("box").style.borderRadius = "100px";
+		} else {
+			document.getElementById("box").style.borderRadius = "0";
+		}
 
+		var top = Math.random();
 
-document.getElementById("box").style.borderRadius="100px";
+		top = top * 300;
 
-				 } else {
+		var left = Math.random();
 
+		left = left * 500;
 
-document.getElementById("box").style.borderRadius="0";
+		document.getElementById("box").style.top = top + "px";
 
-
-				 }
-
-				 var top=Math.random();
-
-				 top=top*300;
-
-				 var left=Math.random();
-
-				 left=left*500;
-
-				 document.getElementById("box").style.top=top+"px";
-
-				 document.getElementById("box").style.left=left+"px";
+		document.getElementById("box").style.left = left + "px";
 
 
-document.getElementById("box").style.backgroundColor=getRandomColor();
+		document.getElementById("box").style.backgroundColor = getRandomColor();
 
-				 document.getElementById("box").style.display="block";
+		document.getElementById("box").style.display = "block";
 
-				 createdTime=Date.now();
+		createdTime = Date.now();
 
-			 }, time);
+	}, time);
 
 }
-document.getElementById("box").onclick=function() {
+document.getElementById("box").onclick = function () {
 
-	 	 clickedTime=Date.now();
+	clickedTime = Date.now();
 
-	 	 reactionTime=(clickedTime-createdTime)/1000;
+	reactionTime = (clickedTime - createdTime) / 1000;
 
-	 	 document.getElementById("time").innerHTML=reactionTime;
+	document.getElementById("time").innerHTML = reactionTime;
 
-	 	 this.style.display="none";
+	this.style.display = "none";
 
-	 	 makeBox();
+	makeBox();
 }
 
 makeBox();
