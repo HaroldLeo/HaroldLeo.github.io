95e454a7a2f45c22fd282ce70bea311472bfaa91
Hongji Liu <h125678@gmail.com>
2020-10-15 20:18:51 -0500

    test heroku

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8888;
 
-app.use(express.static('public'));
+app.use(express.static('./'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
