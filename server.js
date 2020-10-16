54139193fd32f2a0a6aa01cc5fa1b45793478175
Hongji Liu <h125678@gmail.com>
2020-10-15 19:58:42 -0500

    hopefully this fixes the issue

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const PORT = 8888;
 
-app.use(express.static('./'));
+app.use(express.static('public'));
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
